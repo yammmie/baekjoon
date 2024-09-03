3fce4ae4c5f998f74642c72ca9b3ebed6d3fe9ad
yammmie <141018558+yammmie@users.noreply.github.com>
2024-09-03 17:05:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        
+        System.out.println(a + b + c);
+    }
+}
