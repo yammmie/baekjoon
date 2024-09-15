be2f0acb0f83ed3b97f9b779fe29799d46952a68
yammmie <141018558+yammmie@users.noreply.github.com>
2024-09-15 17:03:06 +0900

    [Bronze V] Title: A+B - 4, Time: 116 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+        
+        while((str = br.readLine()) != null) {
+            st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            sb.append(a + b + "\n");
+        }
+        
+        System.out.println(sb);
+    }
+}
