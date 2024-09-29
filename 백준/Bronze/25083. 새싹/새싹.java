5ecc30e830ba0f2bbe07079eb7357cd346447ae9
yammmie <141018558+yammmie@users.noreply.github.com>
2024-09-29 16:56:21 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("         ,r'\"7\n");
+        bw.write("r`-_   ,\'  ,/\n");
+        bw.write(" \\. \". L_r\'\n");
+        bw.write("   `~\\/\n");
+        bw.write("      |\n");
+        bw.write("      |\n");
+        
+        bw.flush();
+        bw.close();
+    }
+}
