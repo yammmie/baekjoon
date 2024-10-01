fb3555552f1c675994621c19d268791012d4d4c4
yammmie <141018558+yammmie@users.noreply.github.com>
2024-10-01 18:44:57 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -10,22 +10,24 @@ public class Main {
         for(int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             
-			if(ch == 'c' && i < str.length() - 1) {
-				if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')
-					i++;
-			} else if(ch == 'd' && i < str.length() - 1) {
-				if(str.charAt(i + 1) == '-')
-                    i++;
-				else if(str.charAt(i + 1) == 'z' && i < str.length() - 2)
-                    if(str.charAt(i + 2) == '=')
-                        i += 2;
-			} else if((ch == 'l' || ch == 'n') && i < str.length() - 1) {
-				if(str.charAt(i + 1) == 'j')
-					i++;
-			} else if((ch == 's' || ch == 'z') && i < str.length() - 1) {
-				if(str.charAt(i + 1) == '=')
-					i++;
-		    }
+            if(i < str.length() - 1) {
+                if(ch == 'c') {
+			    	if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')
+			    		i++;
+		    	} else if(ch == 'd') {
+				    if(str.charAt(i + 1) == '-')
+                        i++;
+			    	else if(str.charAt(i + 1) == 'z' && i < str.length() - 2)
+                        if(str.charAt(i + 2) == '=')
+                            i += 2;
+		    	} else if(ch == 'l' || ch == 'n') {
+			    	if(str.charAt(i + 1) == 'j')
+			    		i++;
+		    	} else if(ch == 's' || ch == 'z') {
+			    	if(str.charAt(i + 1) == '=')
+			    		i++;
+		        }
+            }
         
             cnt++;    
         }
