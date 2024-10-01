f8a0ca8be730098432f47cab28233d9f5f69d249
yammmie <141018558+yammmie@users.noreply.github.com>
2024-10-01 18:47:46 +0900

    Update 크로아티아 알파벳.java

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -12,21 +12,21 @@ public class Main {
             
             if(i < str.length() - 1) {
                 if(ch == 'c') {
-			    	if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')
-			    		i++;
-		    	} else if(ch == 'd') {
-				    if(str.charAt(i + 1) == '-')
-                        i++;
-			    	else if(str.charAt(i + 1) == 'z' && i < str.length() - 2)
-                        if(str.charAt(i + 2) == '=')
-                            i += 2;
-		    	} else if(ch == 'l' || ch == 'n') {
-			    	if(str.charAt(i + 1) == 'j')
-			    		i++;
-		    	} else if(ch == 's' || ch == 'z') {
-			    	if(str.charAt(i + 1) == '=')
-			    		i++;
-		        }
+		    if(str.charAt(i + 1) == '=' || str.charAt(i + 1) == '-')
+		        i++;
+		} else if(ch == 'd') {
+		    if(str.charAt(i + 1) == '-')
+			i++;
+		    else if(str.charAt(i + 1) == 'z' && i < str.length() - 2)
+		        if(str.charAt(i + 2) == '=')
+		            i += 2;
+		} else if(ch == 'l' || ch == 'n') {
+		    if(str.charAt(i + 1) == 'j')
+		        i++;
+		} else if(ch == 's' || ch == 'z') {
+		    if(str.charAt(i + 1) == '=')
+		        i++;
+		}
             }
         
             cnt++;    
