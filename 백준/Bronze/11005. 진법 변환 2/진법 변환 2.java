6a95041042924653d06906553165590671752c94
yammmie <141018558+yammmie@users.noreply.github.com>
2024-10-06 16:49:57 +0900

    [Bronze I] Title: 진법 변환 2, Time: 100 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        while(0 < n) {
+            int tmp = 0;
+            
+            if(n % b < 10)
+                tmp = n % b + '0';
+            else
+                tmp = n % b + 'A' - 10;
+            
+            sb.append((char)tmp);
+            n /= b;
+        }
+        
+        System.out.println(sb.reverse());
+    }
+}
