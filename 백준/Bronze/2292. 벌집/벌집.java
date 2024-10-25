222bba88c4a81bd481eddab4d52a6f740b94490d
yammmie <141018558+yammmie@users.noreply.github.com>
2024-10-25 17:42:52 +0900

    [Bronze II] Title: 벌집, Time: 108 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -5,12 +5,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int cnt = 1;
-        int room = 2;
+        int room = 1;
         
         if(n == 1)
             System.out.println(cnt);
         else {
-            while(room <= n) {
+            while(room < n) {
                 room += cnt * 6;
                 cnt++;
             }
