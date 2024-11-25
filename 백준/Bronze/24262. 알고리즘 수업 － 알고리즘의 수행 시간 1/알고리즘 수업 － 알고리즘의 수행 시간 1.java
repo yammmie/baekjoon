11f74fff6abf10d649ba2686878eeb00f8351fae
yammmie <141018558+yammmie@users.noreply.github.com>
2024-11-25 17:01:52 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 108 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // MenOfPassion 알고리즘
+        // 반복문 X -> 어떤 수를 넣어도 1번만 수행됨
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
