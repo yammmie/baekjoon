7573d4039ac3331968795542d14a6ec2121a3395
yammmie <141018558+yammmie@users.noreply.github.com>
2024-11-25 17:05:35 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // MenOfPassion 알고리즘: for문 1번 수행
+        System.out.println(br.readLine());
+        System.out.println(1);
+    }
+}
